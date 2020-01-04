166232e955630d5f12829e48b81bcf26640c6488
Yuting Zhang <yutingzh@amazon.com>
2020-01-04 13:22:39 -0800

    deep indexing: apply to deep index path

--- zutils/deep_indexing.py
+++ zutils/deep_indexing.py
@@ -3,24 +3,25 @@ import copy
 
 
 __all__ = [
-    'apply_to_index_path',
+    'apply_to_index_paths',
+    'apply_to_single_index_path',
     'INDEX_WILDCARD',
     'KEY_WILDCARD',
 ]
 
 
-class _INDEX_WILDCARD:
+class _IndexWildcard:
     pass
 
 
-INDEX_WILDCARD = _INDEX_WILDCARD()
+INDEX_WILDCARD = _IndexWildcard()
 
 
-class _KEY_WILDCARD:
+class _KeyWildcard:
     pass
 
 
-KEY_WILDCARD = _KEY_WILDCARD()
+KEY_WILDCARD = _KeyWildcard()
 
 
 class _NotSpecified:
@@ -30,18 +31,18 @@ class _NotSpecified:
 INDEX_PATH = Tuple[Any]
 
 
-def apply_to_index_path(a, func: Callable, index_path2extra_args: Dict[INDEX_PATH, Tuple[Any]]):
+def apply_to_index_paths(a, func: Callable, index_path2extra_args: Dict[INDEX_PATH, Tuple[Any]]):
 
-    out = copy.copy(a)
+    b = a
     for index_path, extra_args in index_path2extra_args.items():
-        _apply_to_single_index_path(
-            a, out, func, index_path, extra_args
+        b = apply_to_single_index_path(
+            b, func, index_path, extra_args
         )
-    return out
+    return b
 
 
-def _apply_to_single_index_path(
-        src, dst, func: Callable, index_path: INDEX_PATH, extra_args: Tuple[Any]
+def apply_to_single_index_path(
+        src, func: Callable, index_path: INDEX_PATH, extra_args: Tuple[Any]
 ):
 
     if not isinstance(index_path, tuple):
@@ -49,28 +50,44 @@ def _apply_to_single_index_path(
 
     assert index_path, "at least one index needs to be in the index path"
 
-    pre_dst = _NotSpecified
-    k = _NotSpecified
-    for i, k in enumerate(index_path):
-        pre_dst = dst
-        index_iter = None
-        if isinstance(k, _INDEX_WILDCARD):
-            index_iter = range(len(src))
-        elif isinstance(k, _KEY_WILDCARD):
-            index_iter = src
+    tuple_type = None
+    if isinstance(src, tuple):
+        tuple_type = type(src)
+        dst = list(src)
+    else:
+        dst = copy.copy(src)
 
-        if index_iter is None:
-            if dst[k] is src[k]:
-                dst[k] = copy.copy(dst[k])
-            src = src[k]
-            dst = dst[k]
+    k = index_path[0]
+    index_iter = None
+    if isinstance(k, _IndexWildcard):
+        index_iter = range(len(src))
+    elif isinstance(k, _KeyWildcard):
+        index_iter = src
+
+    if index_iter is not None:
+        for subkey in src:
+            dst[subkey] = apply_to_single_index_path(
+                src, func, (subkey,) + index_path[1:], extra_args
+            )
+    else:
+
+        if len(index_path) > 1:
+            dst[k] = apply_to_single_index_path(
+                src[k], func, index_path[1:], extra_args
+            )
         else:
-            for subkey in src:
-                _apply_to_single_index_path(
-                    src, dst, func, (subkey,) + index_path[i+1:], extra_args
-                )
-            return
+            dst[k] = func(src[k], *extra_args)
 
-    assert pre_dst is not _NotSpecified, "at least one index needs to be in the index path"
+    if tuple_type is not None:
+        dst = tuple_type(dst)
 
-    pre_dst[k] = func(src, *extra_args)
+    return dst
+
+
+def set_to_index_path(a, index_path: INDEX_PATH, value: Any):
+    raise NotImplementedError
+
+
+def get_from_index_path(a, index_path: INDEX_PATH):
+    raise NotImplementedError
+
