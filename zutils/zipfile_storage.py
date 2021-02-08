3bb56c0d6f1f6ac0d6f8a04e0363b2a1d2699f99
Yuting Zhang <yutingzh@amazon.com>
2021-02-08 14:04:04 -0800

    can storage and load int/str in more direct format

--- zutils/zipfile_storage.py
+++ zutils/zipfile_storage.py
@@ -19,20 +19,41 @@ class ZipFileStorage:
         self._pickle_executor = ThreadPoolExecutor(max_workers=num_workers)
         self._cache = dict()
         self._cache_access_lock = Lock()
+        self._key2ext = dict()
+        self._init_key2ext_mapping()
+
+    def _init_key2ext_mapping(self):
+        for fn in self._zf.namelist():
+            ext = None
+            if fn.endswith(".pkl"):
+                ext = ".pkl"
+            elif fn.endswith(".int.txt"):
+                ext = ".int.txt"
+            elif fn.endswith(".str.txt"):
+                ext = ".str.txt"
+            if ext:
+                self._key2ext[fn[:-len(ext)]] = ext
 
     def __getitem__(self, key):
         key = str(key)
         with self._cache_access_lock:
             if key in self._cache:
                 return self._cache[key][0]
-        value = self._zf.read(key + ".pkl")
+        ext = self._key2ext[key]
+        s = self._zf.read(key + ext)
+        if ext == ".int.txt":
+            value = int(s.decode(encoding='UTF-8'))
+        elif ext == ".str.txt":
+            value = s.decode(encoding='UTF-8')
+        else:
+            value = pickle.loads(s)
         return value
 
     def __setitem__(self, key, value):
         assert self._mode != 'r', "cannot set item in read mode"
         key = str(key)
         r = self._pickle_executor.submit(
-            pickle_and_add_to_zip_queue, key, value, self._zipfile_executor,
+            serialize_and_add_to_zip_queue, key, value, self._zipfile_executor,
             self._zf, self._cache, self._cache_access_lock
         )
         with self._cache_access_lock:
@@ -44,12 +65,10 @@ class ZipFileStorage:
 
     def open(self, key, mode='r', *args, **kwargs):
         key = str(key)
-        return self._zf.open(key + ".pkl", mode, *args, **kwargs)
+        return self._zf.open(key + self._key2ext[key], mode, *args, **kwargs)
 
     def keys(self):
-        for k in self._zf.namelist():
-            assert k.endswith(".pkl"), "unrecognized element ids"
-            yield k[:-4]
+        return self._key2ext.keys()
 
     def values(self):
         for k in self.keys():
@@ -81,15 +100,23 @@ class ZipFileStorage:
         self._zf.close()
 
 
-def pickle_and_add_to_zip_queue(
+def serialize_and_add_to_zip_queue(
         key, value, zipfile_executor: ThreadPoolExecutor,
         zf: zipfile.ZipFile, cache: dict, cache_access_lock: Lock
 ):
-    s = pickle.dumps(value, protocol=pickle.HIGHEST_PROTOCOL)
-    zipfile_executor.submit(add_to_zip, key, s, zf, cache, cache_access_lock)
+    if isinstance(value, int):
+        ext = ".int.txt"
+        s = str(value).encode(encoding='UTF-8')
+    elif isinstance(value, str):
+        ext = ".str.txt"
+        s = value.encode(encoding='UTF-8')
+    else:
+        ext = ".pkl"
+        s = pickle.dumps(value, protocol=pickle.HIGHEST_PROTOCOL)
+    zipfile_executor.submit(add_to_zip, key, ext, s, zf, cache, cache_access_lock)
 
 
-def add_to_zip(key, s, zf: zipfile.ZipFile, cache: dict, cache_access_lock: Lock):
-    zf.writestr(key + ".pkl", s)
+def add_to_zip(key, s, ext: str, zf: zipfile.ZipFile, cache: dict, cache_access_lock: Lock):
+    zf.writestr(key + ext, s)
     with cache_access_lock:
         cache.pop(key)
