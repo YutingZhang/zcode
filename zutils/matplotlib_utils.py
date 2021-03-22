1fe5b5dda6a3ba248a2e128b95dbc16a5ba903b4
Yuting Zhang <yutingzh@amazon.com>
2021-03-21 22:40:03 -0400

    bugfix

--- zutils/matplotlib_utils.py
+++ zutils/matplotlib_utils.py
@@ -6,26 +6,27 @@ def preprocess_and_get_figure_filename(fn: str) -> str:
     output_path = fn
     if fn.endswith(".html"):
         rel_output_path = os.path.join(
-            "_figures", os.path.splitext(os.path.basename(fn))[0] + ".jpg"
+            "_figures", os.path.splitext(os.path.basename(os.path.abspath(fn)))[0] + ".jpg"
         )
         output_path = os.path.join(os.path.dirname(fn), rel_output_path)
+        mkpdir_p(output_path)
         with open(fn, "w") as f:
             print("""
-    <html>
-    <header>
-        <style>
-            html {padding: 0; margin: 0}
-            body {padding: 0; margin: 0; overflow-x: hidden;}
-            .main-picture {padding: 0; margin: 0 1%; width: 98%;}
-        </style>
-    </header>
-    <body>
-    <img class="main-picture" src="$$$IMAGE_PATH$$$">
-    </body>
-    </html>
-    """.replace("$$$IMAGE_PATH$$$", rel_output_path),
-                  file=f)
+<html>
+<header>
+    <style>
+        html {padding: 0; margin: 0}
+        body {padding: 0; margin: 0; overflow-x: hidden;}
+        .main-picture {padding: 0; margin: 0 1%; width: 98%;}
+    </style>
+</header>
+<body>
+<img class="main-picture" src="$$$IMAGE_PATH$$$">
+</body>
+</html>
+""".replace("$$$IMAGE_PATH$$$", rel_output_path),file=f)
 
-    mkpdir_p(output_path)
+    else:
+        mkpdir_p(output_path)
 
     return output_path
